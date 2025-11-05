e96b505a5f7c1015fcc8b2a7ea1ef2839942531f
Buggy Burrow <cassy@buggyburrow.com>
2025-11-05 20:08:49 +0900

    Refactor - Remove count from Item, create ItemStack struct

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,161 @@
+use std::fmt;
+
 fn main() {
+    let mut user = User::new(String::from("Buggy"));
+    let mut shop = Shop::new(String::from("Pickle Shop"));
+    
+    let cucumber = Item::new(String::from("Cucumber"), '🥒', 3);
+    println!("{:?}", cucumber);
 
-    let mut user = User::new();
-    let mut pickle_shop = get_pickle_shop_inventory();
+    let mut cucumber_stack = ItemStack::new(cucumber);
+    cucumber_stack.add_one();
+    cucumber_stack.add_one();
+    cucumber_stack.subtract_one();
 
-    pickle_shop.display();
+    let mut inventory = Inventory::new();
+    inventory.0.push(cucumber_stack);
+    
+
+
+    
+    println!("{:?}", inventory);
+
+
 }
 
+/*
 fn get_pickle_shop_inventory() -> Inventory {
     Inventory {
         inventory: vec![
-        Item{name: "Cucumber".into(), visual: '🥒', price: 2, count: 5},
-        Item{name: "Tomato".into(), visual: '🍅', price: 7, count: 3},
-        Item{name: "Ginger".into(), visual: '🫚', price: 3, count: 3},
-        Item{name: "Cabbage".into(), visual: '🥬', price: 4, count: 3},
-        Item{name: "Watermelon".into(), visual: '🍉', price: 6, count: 3},
-        Item{name: "Green Pepper".into(), visual: '🫑', price: 5, count: 3},
-        Item{name: "Onion".into(), visual: '🧅', price: 3, count: 3},
-        Item{name: "Eggplant".into(), visual: '🍆', price: 10, count: 3}, 
-        Item{name: "Carrot".into(), visual: '🥕', price: 4, count: 3},
+        Item{name: "Cucumber".into(), emoji: '🥒', price: 2, stock: 5},
+        Item{name: "Tomato".into(), emoji: '🍅', price: 7, stock: 3},
+        Item{name: "Ginger".into(), emoji: '🫚', price: 3, stock: 3},
+        Item{name: "Cabbage".into(), emoji: '🥬', price: 4, stock: 3},
+        Item{name: "Watermelon".into(), emoji: '🍉', price: 6, stock: 3},
+        Item{name: "Green Pepper".into(), emoji: '🫑', price: 5, stock: 3},
+        Item{name: "Onion".into(), emoji: '🧅', price: 3, stock: 3},
+        Item{name: "Eggplant".into(), emoji: '🍆', price: 10, stock: 3}, 
+        Item{name: "Carrot".into(), emoji: '🥕', price: 4, stock: 3},
     ]
     }
 }
-
+    */
 
 #[derive(Debug)]
+#[derive(Clone)]
 struct Item {
     name: String,
-    visual: char,
+    emoji: char,
     price: u32,
-    count: u32,
 }
 
-
 impl Item {
-    fn increase_count(&mut self) {
-        self.count += 1;
-    }
-    
-    fn decrease_count(&mut self) {
-        if self.count > 0 {
-            self.count -= 1;
+    fn new(name: String, emoji: char, price: u32) -> Self {
+        Self {
+            name,
+            emoji,
+            price, 
         }
-    }
-    
-    fn has_stock(&self) -> bool {
-        self.count >= 1
-    }
-
-    fn display(&self) {
-        println!("{} {} {} Pickle(s)", self.visual, self.count, self.name);
-    }
-
-    fn display_price(&self) {
-        println!("{} {} Pickles cost ${} each", self.visual, self.name, self.price);
     }
 }
 
 #[derive(Debug)]
-struct Inventory {
-    inventory: Vec<Item>
+struct ItemStack {
+    item: Item,
+    stack: u32,
 }
+
+impl ItemStack {
+    fn new(item: Item) -> ItemStack {
+        Self {
+            item: item,
+            stack: 1,
+        }
+    }
+
+    fn add_one(&mut self) {
+        self.stack += 1;
+    }
+
+    fn subtract_one(&mut self) -> Result<(),()> {
+        if self.stack <= 0 {
+            Err(())
+        }
+        else {
+            self.stack -= 1;
+            Ok(())
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Inventory(Vec<ItemStack>);
 
 impl Inventory {
     fn new() -> Inventory {
-        Inventory {
-            inventory: Vec::new()
-        }
+        Inventory(Vec::new())
     }
 
     fn retrieve_item_index(&self, item_name: String) -> isize {
-        
-        for (index , item) in self.inventory.iter().enumerate() {
-            if item.name == item_name {
+        for (index , item_stack) in self.0.iter().enumerate() {
+            if item_stack.item.name == item_name {
                 return index as isize
             }
         }
         -1
     }
 
-    fn display(&self) {
-        for item in &self.inventory {
-            item.display();
-        }
-    }
+    //fn add_item()
+    //fn add_stack()
+}
 
-    fn display_prices(&self) {
-        for item in 
+struct Shop {
+    name: String,
+    inventory: Inventory,
+}
+
+impl Shop {
+    fn new(name: String) -> Shop {
+        Shop {
+            name,
+            inventory: Inventory::new(),
+        }
     }
 }
 
 struct User {
+    name: String,
     cash: u32,
     inventory: Inventory,
 }
 
 impl User {
-    fn new() -> Self {
+    fn new(name: String) -> Self {
         User {
+            name,
             cash: 100,
             inventory: Inventory::new(),
         }
     }
+/* 
+fn add_item(&self, &item: Item) {
+    let item_index = self.inventory.retrieve_item_index(item.name);
+    if item_index < 0 {
+        //self.inventory.0.push(ItemStack)
+    }
+}
+*/
 
     fn pay(&mut self, price: u32) {
         self.cash -= price;
+    }
+
+    fn can_pay(&self, price: u32) -> bool {
+        self.cash >= price
+    }
+
+    fn display_cash(&self) {
+        println!("You have ${}", self.cash)
     }
 
 }
